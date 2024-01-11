6e0466f109e6c53795d17af25ec125e1f6a0f3ab
Erdinc Mutlu <erdincmutlu@gmail.com>
2024-01-11 19:03:25 +0000

    Middleware functions skeletons

--- server/middleware/middleware.go
+++ server/middleware/middleware.go
@@ -1,1 +1,33 @@
 package middleware
+
+import (
+	"net/http"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var collection *mongo.Collection
+
+func GetAllTasks(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func CreateTask(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func TaskComplete(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func UndoTask(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func DeleteTask(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func DeleteAllTasks(w http.ResponseWriter, r *http.Request) {
+
+}
--- server/router/router.go
+++ server/router/router.go
@@ -9,7 +9,7 @@ func Router() *mux.Router {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/api/task", middleware.GetAllTasks).Methods("GET", "OPTIONS")
-	router.HandleFunc("/api/tasks", middleware.CreateTasks).Methods("POST", "OPTIONS")
+	router.HandleFunc("/api/tasks", middleware.CreateTask).Methods("POST", "OPTIONS")
 	router.HandleFunc("/api/tasks/{id}", middleware.TaskComplete).Methods("PUT", "OPTIONS")
 	router.HandleFunc("/api/undoTask/{id}", middleware.UndoTask).Methods("PUT", "OPTIONS")
 	router.HandleFunc("/api/deleteTask/{id}", middleware.DeleteTask).Methods("DELETE", "OPTIONS")
