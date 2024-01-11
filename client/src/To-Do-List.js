952deddd705338313b844838e403a4c352ec3d3c
Erdinc Mutlu <erdincmutlu@gmail.com>
2024-01-11 18:13:24 +0000

    Add todo list

---
+++ client/src/To-Do-List.js
@@ -0,0 +1,34 @@
+import React,{Component} from "react";
+import axios from "axios";
+import {Card, Header, Form, Input, Icon} from "semantic-ui-react";
+
+let endpoint = "http://localhost:9000";
+
+class ToDoList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      task:"",
+      items:[],
+    };
+  }
+
+  ComponentDidMount() {
+    this.getTask();
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="row">
+            <Header className="header" as="h2" color="lightblue">
+                To Do List
+            </Header>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default TodoList;
