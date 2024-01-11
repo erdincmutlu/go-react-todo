6b6eba29a4947797346f50301949535657a7d2ab
Erdinc Mutlu <erdincmutlu@gmail.com>
2024-01-11 19:20:33 +0000

    Create db instance

--- server/middleware/middleware.go
+++ server/middleware/middleware.go
@@ -1,11 +1,15 @@
 package middleware
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var collection *mongo.Collection
@@ -16,10 +20,33 @@ func init() {
 }
 
 func loadTheEnv() {
-	err := godotenv.Load(".env")
+	err := godotenv.Load("../.env")
 	if err != nil {
 		log.Fatalf("Error loading the .env file: %s", err.Error())
 	}
+}
+
+func createDBInstance() {
+	connectionString := os.Getenv("DB_URI")
+	dbName := os.Getenv("DB_NAME")
+	collectionName := os.Getenv("DB_COLLECTION_NAME")
+
+	clientOptions := options.Client().ApplyURI(connectionString)
+
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("connected to mongodb")
+
+	collection = client.Database(dbName).Collection(collectionName)
+	fmt.Println("Collection instance created")
 }
 
 func GetAllTasks(w http.ResponseWriter, r *http.Request) {
