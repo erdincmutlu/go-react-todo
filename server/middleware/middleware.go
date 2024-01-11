10064605cc13d9616c21317375aa1e54feaa8800
Erdinc Mutlu <erdincmutlu@gmail.com>
2024-01-11 19:08:20 +0000

    Load from environment

--- server/middleware/middleware.go
+++ server/middleware/middleware.go
@@ -1,12 +1,26 @@
 package middleware
 
 import (
+	"log"
 	"net/http"
 
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 var collection *mongo.Collection
+
+func init() {
+	loadTheEnv()
+	createDBInstance()
+}
+
+func loadTheEnv() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading the .env file: %s", err.Error())
+	}
+}
 
 func GetAllTasks(w http.ResponseWriter, r *http.Request) {
 
